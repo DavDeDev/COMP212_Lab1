d60959bae15fb99d15667074a378d764f25d4887
David02P <pietrocoladavid@gmail.com>
2023-01-28 17:35:36 -0500

    feat: added q1 and q2

---
+++ exercise1/LinkedlistAndArrays.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavidPietrocola_Lab1.exercise1
+{
+    internal class LinkedlistAndArrays
+    {
+        public static void Main(string[] args)
+        {
+            //TODO: Use example(s) to illustrate the differences between array and linked list
+            
+            // arrays are fixed in size, which means that you can't add values after its creation
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Array: ");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+
+            // LinkedList can be created with a fixed size or without a size
+            LinkedList<int> numbers = new LinkedList<int>();
+            numbers.AddLast(1);
+            numbers.AddLast(2);
+            numbers.AddLast(3);
+            numbers.AddLast(4);
+            numbers.AddLast(5);
+            numbers.RemoveLast();
+            // We can add and remove values after its initialization
+
+            Console.WriteLine("LinkedList: ");
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Prevent the console from closing
+            Console.ReadKey();
+        }
+
+
+    }
+
+
+}
---
+++ exercise1/StackAndQueue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavidPietrocola_Lab1.exercise1
+{
+    internal class Class1
+    {
+        //Use example(s) to illustrate the features of stack and queue
+        public static void Main(string[] args)
+        {
+            // Stack is a LIFO (Last In First Out) data structure
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            // This will remove the last value added to the stack
+            stack.Pop();
+
+            Console.WriteLine("Stack: ");
+            foreach (int i in stack)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            // Queue is a FIFO (First In First Out) data structure
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            // This will remove the first value added to the queue
+            queue.Dequeue();
+
+            Console.WriteLine("Queue: ");
+            foreach (int i in queue)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Prevent the console from closing
+            Console.ReadKey();
+        }
+    }
+}
