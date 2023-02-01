75bb40069815d9896ed8a0e195a995419d4d9232
DavDeDev <pietrocoladavid@gmail.com>
2023-02-01 12:22:36 -0500

    fix: second exercise

--- exercise2/CountWords.cs
+++ exercise2/CountWords.cs
@@ -5,22 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 namespace CountWords
 {
-class Program
-{
-static void Main(string[] args)
-{
-string str;
-Console.Write("\nEnter a string: ");
-str = Console.ReadLine();
-StringBuilder sb = new StringBuilder(str);
-IEnumerable<string> words = sb.ToString().Split(' ');
-int count = 0;
-foreach (var c in words)
-{
-count++;
-}
-Console.WriteLine("\nThe number of words contained in sb is "+count);
-Console.ReadKey();
-}
-}
+    class Program
+    {
+        //Implement an extension method for class StringBuilder to count the number of words contained in a
+        //StringBuilder object. For example, if a StringBuilder object sb = ”This is to test whether the extension
+        //method count can return a right answer or not”, the number of words contained in sb is 16.
+        static void Main(string[] args)
+        {
+            string str;
+            Console.Write("\nEnter a string: ");
+            str = Console.ReadLine();
+            StringBuilder sb = new StringBuilder(str);
+            Console.WriteLine("Number of words in the string: " + CountWords(sb));
+            Console.ReadKey();
+        }
+
+        static int CountWords(StringBuilder sb)
+        {
+            IEnumerable<string> words = sb.ToString().Split(' ');
+            int count = 0;
+            foreach (var c in words)
+            {
+                count++;
+            }
+            return count;
+        }
+    }
 }
